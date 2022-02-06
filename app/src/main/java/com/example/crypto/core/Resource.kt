48efa987919d39e63a4b40f55f5994ce117bd408
John_Dominic <johndominicjasmin@gmail.com>
2022-02-07 01:04:02 +0800

    Change Warning class to Loading

--- app/src/main/java/com/example/crypto/core/Resource.kt
+++ app/src/main/java/com/example/crypto/core/Resource.kt
@@ -2,7 +2,7 @@ package com.example.crypto.core
 
 sealed class Resource <T> (val data:T? = null, val message:String? = null){
     class Success<T>(data:T):Resource<T>(data)
-    class Error<T>(message: String, data: T? = null):Resource<T>(data,message)
-    class Warning<T>(data :T? = null):Resource<T>(data)
+    class Error<T>(data: T? = null , message: String):Resource<T>(data,message)
+    class Loading<T>(data :T? = null):Resource<T>(data)
 
 }
