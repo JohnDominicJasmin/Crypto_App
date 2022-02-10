28aac38d7edd925d8d3658f256c3de77974cf08f
John_Dominic <johndominicjasmin@gmail.com>
2022-02-10 21:45:04 +0800

    Setup NavController for navigation & add Screen class

--- app/src/main/java/com/example/crypto/feature_coin/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/crypto/feature_coin/presentation/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.crypto.feature_coin.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,16 +10,39 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.crypto.common.CoinConstants
+import com.example.crypto.feature_coin.presentation.ui.coin_detail.components.CoinDetailScreen
+import com.example.crypto.feature_coin.presentation.ui.coin_list.components.CoinListScreen
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoTheme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
+                    val navController  = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route) {
+
+                        composable(route = Screen.CoinListScreen.route){
+                            Timber.e("${this.javaClass.name}: Navigation Arguments:"+"${it.arguments?.getString(CoinConstants.COIN_ID)}")
+                            CoinListScreen(navController = navController)
+                        }
+
+                        composable(route = Screen.CoinDetailScreen.route + "/{${CoinConstants.COIN_ID}}"){
+                            CoinDetailScreen()
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/example/crypto/feature_coin/presentation/ui/Screen.kt
+++ app/src/main/java/com/example/crypto/feature_coin/presentation/ui/Screen.kt
@@ -2,7 +2,7 @@ package com.example.crypto.feature_coin.presentation.ui
 
 sealed class Screen(val route:String){
 
-    object CoinListScreen:Screen("coin_list_screen")
-    object CoinDetailScreen:Screen("coin_detail_screen")
+    object CoinListScreen:Screen(route = "coin_list_screen")
+    object CoinDetailScreen:Screen(route ="coin_detail_screen")
 
 }
