c7b216713e4e093a708698430a2be6e1a1a2ce35
John_Dominic <johndominicjasmin@gmail.com>
2022-02-11 03:00:53 +0800

    Fix Serialization issue

--- app/src/main/java/com/example/crypto/common/CoinConstants.kt
+++ app/src/main/java/com/example/crypto/common/CoinConstants.kt
@@ -2,5 +2,5 @@ package com.example.crypto.common
 
 object CoinConstants {
     const val BASE_URL = "https://api.coinpaprika.com/"
-    const val COIN_ID = "coin_Id"
+    const val COIN_ID = "coinId"
 }
