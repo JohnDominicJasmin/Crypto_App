6cba7d279d93b9ff248160c3e97f85c02608e848
John_Dominic <johndominicjasmin@gmail.com>
2022-02-11 03:00:53 +0800

    Fix Deserialization issue

--- app/src/main/java/com/example/crypto/feature_coin/data/remote/dto/CoinDetailDto.kt
+++ app/src/main/java/com/example/crypto/feature_coin/data/remote/dto/CoinDetailDto.kt
@@ -1,27 +1,41 @@
 package com.example.crypto.feature_coin.data.remote.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class CoinDetailDto(
-    val description: String,
-    val development_status: String,
-    val first_data_at: String,
-    val hardware_wallet: Boolean,
-    val hash_algorithm: String,
+    val description: String? = null,
+    @SerializedName("development_status")
+    val developmentStatus: String,
+    @SerializedName("first_data_at")
+    val firstDataAt: String,
+    @SerializedName("hardware_wallet")
+    val hardwareWallet: Boolean,
+    @SerializedName("hash_algorithm")
+    val hashAlgorithm: String,
     val id: String,
-    val is_active: Boolean,
-    val is_new: Boolean,
-    val last_data_at: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    @SerializedName("last_data_at")
+    val lastDataAt: String,
     val links: Links,
-    val links_extended: List<LinksExtended>,
+    @SerializedName("links_extended")
+    val linksExtended: List<LinksExtended>,
     val message: String,
     val name: String,
-    val open_source: Boolean,
-    val org_structure: String,
-    val proof_type: String,
+    @SerializedName("open_source")
+    val openSource: Boolean,
+    @SerializedName("org_structure")
+    val orgStructure: String,
+    @SerializedName("proof_type")
+    val proofType: String,
     val rank: Int,
-    val started_at: String,
+    @SerializedName("started_at")
+    val startedAt: String,
     val symbol: String,
-    val tags: List<Tag>,
-    val teamMembers: List<TeamMember>,
+    val tags: List<Tag>? = null,
+    val team: List<TeamMember>? = null,
     val type: String,
     val whitepaper: Whitepaper
 )
--- app/src/main/java/com/example/crypto/feature_coin/domain/model/CoinDetail.kt
+++ app/src/main/java/com/example/crypto/feature_coin/domain/model/CoinDetail.kt
@@ -5,10 +5,10 @@ import com.example.crypto.feature_coin.data.remote.dto.TeamMember
 data class CoinDetail(
     val coinId:String,
     val name:String,
-    val description:String,
+    val description:String? = null,
     val symbol:String,
     val rank:Int,
-    val team:List<TeamMember>,
+    val team:List<TeamMember>? = null,
     val isActive:Boolean,
-    val tags:List<String>
+    val tags:List<String>? = null
 )
