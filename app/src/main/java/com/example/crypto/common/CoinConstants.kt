865016f3cc46fbfa064eae67e24be17e5d00fc1c
John_Dominic <johndominicjasmin@gmail.com>
2022-06-22 23:09:58 +0800

    Fix 404 crypto icon api.

--- app/src/main/java/com/example/crypto/common/CoinConstants.kt
+++ app/src/main/java/com/example/crypto/common/CoinConstants.kt
@@ -2,6 +2,7 @@ package com.example.crypto.common
 
 object CoinConstants {
     const val COIN_PAPRIKA_API_BASE_URL = "https://api.coinpaprika.com/"
-    const val CRYPTO_ICON_BASE_URL = "https://cryptoicon-api.vercel.app/api/icon/"
+    const val CRYPTO_ICON_BASE_URL = "https://raw.githubusercontent.com/jsupa/crypto-icons/main/icons/"
     const val COIN_ID = "coinId"
+    const val GOOGLE_SEARCH_QUERY = "https://www.google.com/search?q="
 }
