c35f55e7cfc62538ac1d38675538b905960abdbc
John_Dominic <johndominicjasmin@gmail.com>
2022-06-22 23:09:40 +0800

    Add icon for error.

--- app/src/main/java/com/example/crypto/feature_coin/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/example/crypto/feature_coin/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.example.crypto.feature_coin.presentation.ui
+package com.example.crypto.feature_coin.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
